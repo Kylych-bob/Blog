f129e50de03ccce5b465ba8b0a5a714c8bb1f25b
Mirbekov <@kylych-bob>
2022-07-21 18:40:15 +0600

    Stemming_&_ranking_results

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'taggit',
     'django.contrib.sites',
-    'django.contrib.sitemaps'
+    'django.contrib.sitemaps',
+    # Search lookups
+    'django.contrib.postgres'
 ]
 
 MIDDLEWARE = [
@@ -81,10 +83,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'mirbekov',
+        'PASSWORD': '1'
     }
 }
 
